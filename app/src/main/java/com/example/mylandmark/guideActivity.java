aebfa02ee1d784c68436f6a36fe07a57bf8f2e76
dbstmdgus <yuns7013@gmail.com>
2022-05-30 14:07:51 +0900

    0530

--- app/src/main/java/com/example/mylandmark/guideActivity.java
+++ app/src/main/java/com/example/mylandmark/guideActivity.java
@@ -2,7 +2,11 @@ package com.example.mylandmark;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class guideActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class guideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
+        findViewById(R.id.img_map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(guideActivity.this, "이미지 클릭", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("geo:37.5711455!4d126.9883295"));
+                startActivity(intent);
+            }
+        });
     }
 }
